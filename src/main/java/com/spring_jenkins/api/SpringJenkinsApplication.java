2bd5d77a9dc6cb21387630c99eb701176e990e61
lmembe <lestermembe@gmail.com>
2022-06-21 10:20:27 +0100

    Changed message in SpringApplication class file.

--- src/main/java/com/spring_jenkins/api/SpringJenkinsApplication.java
+++ src/main/java/com/spring_jenkins/api/SpringJenkinsApplication.java
@@ -15,7 +15,7 @@ public class SpringJenkinsApplication {
 	}
 	
 	public static void main(String[] args) {
-		logger.info("Application executed...");
+		logger.info("Application executing...");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
